9d8ffc53a98be9bebaedddfde22fef1fb844bfb0
Aiden Marques <aiden.marques94@gmail.com>
2020-09-26 13:13:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+#require 'pry'
+class School
+  attr_accessor :name, :roster
+  
+ 
+  
+  def initialize (name)
+    @name=name
+    @roster= {}
+  end
+  
+  def add_student(student_name,grade)
+    #check if key exists. If we know our key we dont have to look at every number
+   if roster[grade]
+     @roster[grade] << student_name
+   else 
+    @roster[grade] = []
+    @roster[grade] << student_name
+  end 
+  end
+  
+  def grade(grade_level)
+    @roster[grade_level]    
+  end
+  
+  def sort 
+    sorted_hash = {}
+    roster.each do |grade,students| 
+  end 
+  
+end 
+
+school = School.new("Bayside High School")
